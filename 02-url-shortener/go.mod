b9f9e5e10ffc5c18db9a8d8d2f50397364c973b5
hfarhi <hicham.farhi@cgi.com>
2024-09-25 12:36:08 +0100

    Adding parseyaml and buildmap funcs

--- 02-url-shortener/go.mod
+++ 02-url-shortener/go.mod
@@ -1,3 +1,5 @@
 module github.com/hichamov/gophercises/02-url-shortener
 
 go 1.22.5
+
+require gopkg.in/yaml.v3 v3.0.1
--- 02-url-shortener/main/main.go
+++ 02-url-shortener/main/main.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	_ "fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v3"
 	// myrandompackage "github.com/hichamov/gophercises/02-url-shortener"
 )
+
+type Route struct {
+	Routes []map[string]string
+}
 
 func main() {
 
 	// This map will be used to create a handler
-	urlmap := map[string]string{
-		"/godoc": "https://go.dev/doc/",
-		"/pydoc": "https://docs.python.org/3/",
-	}
+	// urlmap := map[string]string{
+	// 	"/godoc": "https://go.dev/doc/",
+	// 	"/pydoc": "https://docs.python.org/3/",
+	// }
 
 	// // This is the fallback handler
 	fallbackHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Home Page!"))
 	})
 
-	mainHandler := MapHandler(urlmap, fallbackHandler)
+	yamlfile, err := os.ReadFile("routes.yml")
+	if err != nil {
+		log.Println(err)
+	}
+
+	r := parseYaml(yamlfile)
+	finalmap := buildMap(r)
+	
+	// YAMLHandler(yamlfile, fallbackHandler)
+
+	// mainHandler := MapHandler(urlmap, fallbackHandler)
+	mainHandler := MapHandler(finalmap, fallbackHandler)
+
 	
 	log.Fatal(http.ListenAndServe(":8080", mainHandler))
 }
@@ -35,4 +55,41 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+// func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+// 	parsedyaml := parseYaml(yaml)
+
+// 	return nil, nil
+// }
+
+func parseYaml(yamlstring []byte) Route{
+	var parsedyaml Route
+	err := yaml.Unmarshal(yamlstring, &parsedyaml)
+	if err != nil {
+		log.Println(err)
+	}
+	return parsedyaml
+}
+
+func buildMap(r Route) map[string]string{
+	finalmap := make(map[string]string)
+	var gkey, gval string
+	
+	for _, val := range r.Routes {
+		for mkey, mval := range val {
+
+			if mkey == "name" {
+				gkey = mval
+			}
+
+			if mkey == "target" {
+				gval = mval
+			}
+
+			finalmap[gkey] = gval
+		}
+	}
+
+	return finalmap
 }
