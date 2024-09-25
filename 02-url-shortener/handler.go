7c216be39a4573f8847cef926d0415671a352856
hfarhi <hicham.farhi@cgi.com>
2024-09-25 22:42:47 +0100

    Create handler.go

--- 02-url-shortener/handler.go
+++ 02-url-shortener/handler.go
@@ -2,13 +2,62 @@ package shortener
 
 import (
 	"net/http"
+	"log"
+	"gopkg.in/yaml.v3"
 )
 
+type Route struct {
+	Routes []map[string]string
+}
+
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
 
-	Handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("farhi"))
-	})
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusSeeOther)
+		}
 
-	return Handler
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func YAMLHandler(yamlcontent []byte, fallback http.Handler) (http.HandlerFunc, error) {
+
+	parsedyaml := parseYaml(yamlcontent)
+	finalmap := buildMap(parsedyaml)
+
+	return MapHandler(finalmap, fallback), nil
+}
+
+func parseYaml(yamlstring []byte) Route{
+	var parsedyaml Route
+	err := yaml.Unmarshal(yamlstring, &parsedyaml)
+	if err != nil {
+		log.Println(err)
+	}
+	return parsedyaml
+}
+
+func buildMap(r Route) map[string]string{
+	finalmap := make(map[string]string)
+	var gkey, gval string
+	
+	for _, val := range r.Routes {
+		for mkey, mval := range val {
+
+			if mkey == "name" {
+				gkey = mval
+			}
+
+			if mkey == "target" {
+				gval = mval
+			}
+
+			finalmap[gkey] = gval
+		}
+	}
+
+	return finalmap
 }
--- 02-url-shortener/main/main.go
+++ 02-url-shortener/main/main.go
@@ -5,13 +5,8 @@ import (
 	"net/http"
 	"os"
 
-	"gopkg.in/yaml.v3"
-	// myrandompackage "github.com/hichamov/gophercises/02-url-shortener"
+	mycustomshortner "github.com/hichamov/gophercises/02-url-shortener"
 )
-
-type Route struct {
-	Routes []map[string]string
-}
 
 func main() {
 
@@ -25,59 +20,7 @@ func main() {
 		log.Println(err)
 	}
 	
-	mainHandler, _ := YAMLHandler(yamlfile, fallbackHandler)
+	mainHandler, _ := mycustomshortner.YAMLHandler(yamlfile, fallbackHandler)
 	
 	log.Fatal(http.ListenAndServe(":8080", mainHandler))
-}
-
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-
-		if dest, ok := pathsToUrls[path]; ok {
-			http.Redirect(w, r, dest, http.StatusSeeOther)
-		}
-
-		fallback.ServeHTTP(w, r)
-	}
-}
-
-func YAMLHandler(yamlcontent []byte, fallback http.Handler) (http.HandlerFunc, error) {
-
-	parsedyaml := parseYaml(yamlcontent)
-	finalmap := buildMap(parsedyaml)
-
-	return MapHandler(finalmap, fallback), nil
-}
-
-func parseYaml(yamlstring []byte) Route{
-	var parsedyaml Route
-	err := yaml.Unmarshal(yamlstring, &parsedyaml)
-	if err != nil {
-		log.Println(err)
-	}
-	return parsedyaml
-}
-
-func buildMap(r Route) map[string]string{
-	finalmap := make(map[string]string)
-	var gkey, gval string
-	
-	for _, val := range r.Routes {
-		for mkey, mval := range val {
-
-			if mkey == "name" {
-				gkey = mval
-			}
-
-			if mkey == "target" {
-				gval = mval
-			}
-
-			finalmap[gkey] = gval
-		}
-	}
-
-	return finalmap
 }
